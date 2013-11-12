925f679dd6045f65604785590b539c28fbf7992a
Steph Tyszka <steph@bootsa.net>
2013-11-12 16:25:07 +0000

    swapped motor pins around to drive the correct motors

--- lib/move.py
+++ lib/move.py
@@ -15,12 +15,12 @@ PWM.set_loglevel(PWM.LOG_LEVEL_ERRORS)
             
 #pins
 #i2c pins can be enabled high at boot - see http://www.raspberrypi.org/phpBB3/viewtopic.php?f=44&t=35321
-_l_enable_pin = 4
-_l_forward_pin = 3
-_l_backward_pin = 2
-_r_enable_pin = 17
-_r_forward_pin = 27
-_r_backward_pin = 22
+_l_enable_pin = 17
+_l_forward_pin = 27
+_l_backward_pin = 22
+_r_enable_pin = 4
+_r_forward_pin = 3
+_r_backward_pin = 2
 
 #constants
 LEFT = 1
@@ -43,7 +43,7 @@ PWM.add_channel_pulse(_dma_r,_r_enable_pin,0,0)
 
 def stop_all():
     """Stop all movement."""
-    print "stop"
+    # print "stop"
     PWM.clear_channel_gpio(_dma_l, _l_enable_pin)
     PWM.clear_channel_gpio(_dma_r, _r_enable_pin)
 
@@ -51,13 +51,13 @@ def stop_all():
 def drive(wheel,speed):
     """Drive each wheel separately."""
     if wheel == LEFT:
-        print "left",
+        # print "left",
         dma = _dma_l
         enable_pin = _l_enable_pin
         forward_pin = _l_forward_pin
         backward_pin = _l_backward_pin
     elif wheel == RIGHT:
-        print "right",
+        # print "right",
         dma = _dma_r
         enable_pin = _r_enable_pin
         forward_pin = _r_forward_pin
@@ -73,17 +73,17 @@ def drive(wheel,speed):
     if speed > 0:
         RPIO.output(forward_pin, True)
         RPIO.output(backward_pin, False)
-        print "forward",
+        # print "forward",
     elif speed < 0:
         RPIO.output(forward_pin, False)
         RPIO.output(backward_pin, True)
-        print "backward",
+        # print "backward",
     else:
-        print "stop",
+        # print "stop",
 
     pwm_amount = int(abs(speed * (1999/100)))
 
-    print "pwm:", pwm_amount
+    # print "pwm:", pwm_amount
     PWM.add_channel_pulse(dma,enable_pin,0,pwm_amount)
 
 def moveme(lSpd=100,rSpd=100,ptime=1):
